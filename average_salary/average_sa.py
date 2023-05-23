bff8a1dfd37ab1a2fd5aa8a83eb0791701d888ca
Holison <kobbyholison2002@gmail.com>
2023-05-23 16:02:20 +0000

    average_salary

---
+++ average_salary/average_sa.py
@@ -0,0 +1,16 @@
+class Solution:
+    def average(self,salary) -> float:
+        sum = 0
+        average = 0
+        for i in range(1,len(salary)):
+            sum += salary[i]
+        average = sum / len(salary)
+        return average
+    
+
+
+
+mysoln = Solution()
+print(mysoln.average([4000,3000,1000,2000]))
+print(mysoln.average([1000,2000,3000]))
+print(mysoln.average([6000,5000,4000,3000,2000,1000]))

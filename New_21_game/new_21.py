0371f2a8a2c3cce3df9c8cc5b2ef8593e345eb2c
Holison <kobbyholison2002@gmail.com>
2023-05-26 11:46:05 +0000

    new 21 game implementation

--- New_21_game/new_21.py
+++ New_21_game/new_21.py
@@ -1,11 +1,23 @@
-import random as rd
 class Solution:
     def new21Game(self, n: int, k: int, maxPts: int) -> float:
-        points = 0
-        # generate a random integer
-        while (points < k):
-            random_int = rd.randint(1,maxPts)
-            points += random_int
+        dp = [0.0] * (k + maxPts)  # Create a dynamic programming array
+        
+        for i in range(k, min(n, k + maxPts - 1) + 1):
+            dp[i] = 1.0  # Initialize the probabilities for points between k and min(n, k + maxPts - 1) to 1.0
+            
+        dp[k - 1] = min(n - k + 1, maxPts) / maxPts  # Calculate the probability for k - 1
+        
+        for i in range(k - 2, -1, -1):
+            dp[i] = dp[i + 1] - (dp[i + maxPts + 1] - dp[i + 1]) / maxPts  # Calculate the probabilities for points from k - 2 to 0
+        
+        return dp[0]  # Return the probability of having 0 or fewer points
+
+
+
+mySoln = Solution()
+print(mySoln.new21Game(10, 1, 10))  # 1.0
+print(mySoln.new21Game(6, 1, 10))  # 0.6
+print(mySoln.new21Game(21, 17, 10))  # 0.73278
 
 
 

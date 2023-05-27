17fcc3e9799648b5ff94cb3b088ea31929e7a44d
Holison <kobbyholison2002@gmail.com>
2023-05-27 17:10:17 +0000

    add condition for x = 1

--- sqrt_x/sqrt.py
+++ sqrt_x/sqrt.py
@@ -2,6 +2,8 @@ class solution:
     def mySqrt(self, x: int) -> int:
         if x == 0:
             return 0
+        elif x == 1:
+            return 1
         else:
             low  = 0
             high = x

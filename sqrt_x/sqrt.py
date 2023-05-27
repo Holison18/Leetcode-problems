155f689226fd319568fb3c808785621628451f64
Holison <kobbyholison2002@gmail.com>
2023-05-27 17:07:47 +0000

    sqrt final

--- sqrt_x/sqrt.py
+++ sqrt_x/sqrt.py
@@ -2,17 +2,22 @@ class solution:
     def mySqrt(self, x: int) -> int:
         if x == 0:
             return 0
-        
-        low  = 0
-        high = x
+        else:
+            low  = 0
+            high = x
 
-        # run the code until the desired value is found
-        while low <= high:
-            mid = (low + high) // 2
-            square = mid * mid
+            # run the code until the desired value is found
+            while low < high:
+                mid = (low + high) // 2
+                square = mid * mid
 
-            if square == x:
-                return mid
+                if square == x:
+                    return mid
+                elif square < x:
+                    low = mid + 1
+                else:
+                    high = mid
+            return low - 1
         
     
 

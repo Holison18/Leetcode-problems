45483e9b556945d5bae8c825fa0eecedda540bc2
Holison <kobbyholison2002@gmail.com>
2023-05-29 11:51:08 +0000

    Count and Say program

--- count&Say/count&Say.py
+++ count&Say/count&Say.py
@@ -5,8 +5,15 @@ class Solution:
 
         digit_count = {}
 
+        # store the values and their counts in the digit_count dictionary
         for digit in number_str:
             if digit in digit_count:
                 digit_count[digit] += 1
             else:
                 digit_count[digit] = 1
+
+        newDigit = ""
+        # now concatenate the digit and it's count
+        for digit,count in digit_count.items():
+            newDigit += str(digit_count[digit]) + str(digit_count[count])
+

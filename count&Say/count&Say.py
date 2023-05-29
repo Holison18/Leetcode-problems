ad337c506c68fc056677180aacae6aefb4fe6c7b
Holison <kobbyholison2002@gmail.com>
2023-05-29 12:22:38 +0000

    count and say program

--- count&Say/count&Say.py
+++ count&Say/count&Say.py
@@ -1,19 +1,28 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
         # convert the number to string
-        number_str = str(n)
+        if n == 1:
+            return "1"
+        else:
+            number_str = str(n)
 
-        digit_count = {}
+            digit_count = {}
 
-        # store the values and their counts in the digit_count dictionary
-        for digit in number_str:
-            if digit in digit_count:
-                digit_count[digit] += 1
-            else:
-                digit_count[digit] = 1
+            # store the values and their counts in the digit_count dictionary
+            for digit in number_str:
+                if digit in digit_count:
+                    digit_count[digit] += 1
+                else:
+                    digit_count[digit] = 1
 
-        newDigit = ""
-        # now concatenate the digit and it's count
-        for digit,count in digit_count.items():
-            newDigit += str(digit_count[digit]) + str(digit_count[count])
+            newDigit = ""
+            # now concatenate the digit and it's count
+            for digit,count in digit_count.items():
+                newDigit += str(count) + str(digit)
+            return newDigit
 
+
+mySoln = Solution()
+print(mySoln.countAndSay(1))  # 1
+print(mySoln.countAndSay(2))  # 11
+print(mySoln.countAndSay(333221))  # 331221

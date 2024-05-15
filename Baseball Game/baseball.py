31ff27b6dd05d77457748adcf2251355b4c840ce
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-05-15 06:38:39 +0000

    baseball.py

--- Baseball Game/baseball.py
+++ Baseball Game/baseball.py
@@ -2,14 +2,12 @@ class Solution(object):
     def CalPoints(self, operations):
         record  = []
         for element in operations:
-            if element.isdigit():
-                record.append(element)
+            if element.lstrip('-').isdigit():
+                record.append(int(element))
             elif element == '+':
-                new_record = record[record.index(element)] + record[record.index(element) - 1]
-                record.append(new_record)
+                record.append(record[-1] + record[-2])
             elif element == 'D':
-                new_record = 2 * record[record.index(element) - 1]
-                record.append(new_record)
+                record.append(2 * record[-1])
             elif element == 'C':
                 record.pop()
         return sum(record)
@@ -17,6 +15,8 @@ class Solution(object):
 
 mySoln = Solution()
 print(mySoln.CalPoints(['5','2','C','D','+']))
+print(mySoln.CalPoints(["5","-2","4","C","D","9","+","+"]))
+print(mySoln.CalPoints(["1","C"]))
 
             
 

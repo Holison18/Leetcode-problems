7503dc671c462274cde0ccd25b9a25be4e2edf5c
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-05-16 15:22:58 +0000

    crawler log folder

--- Crawler Log Folder/crawler.py
+++ Crawler Log Folder/crawler.py
@@ -1,4 +1,19 @@
 class Solution(object):
-    def crawlerLogFolder(self, log):
-        min_operations = 0
-        
+    def crawlerLogFolder(self, logs):
+        # declare a variable to store the current directory (0 for root folder)
+        stack = []
+        for log in logs:
+            if log == "../":
+                if len(stack) != 0:
+                    stack.pop()
+            elif log == "./":
+                continue
+            else:
+                stack.append(log)
+        return len(stack)
+
+
+mySoln = Solution()
+print(mySoln.crawlerLogFolder(["d1/", "d2/", "../", "d21/", "./"]))
+print(mySoln.crawlerLogFolder(["d1/","d2/","./","d3/","../","d31/"]))
+print(mySoln.crawlerLogFolder(["d1/","../","../","../"]))

81f63684ee4c8bc089b492b3dd3faed017c236d9
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-05-20 20:07:23 +0000

    Number of 1 bits

--- Number of 1 bits/hamming_weights.py
+++ Number of 1 bits/hamming_weights.py
@@ -1,7 +1,19 @@
 class Solution(object):
     def number_of_set_bit(self, digit):
+        # convert the decimal number into a binary number
         binary = bin(digit).replace("0b","")
-        print(binary)
+        # convert binary into a string
+        binstr = str(binary)
+        # declare a variable count to store the number of 1 bits
+        count = 0
+
+        for value in binstr:
+            if value == "1":
+                count += 1
+        return count
+
 
 mySoln = Solution()
-mySoln.number_of_set_bit(25)
+print(mySoln.number_of_set_bit(25))
+print(mySoln.number_of_set_bit(11))
+print(mySoln.number_of_set_bit(2147483645))

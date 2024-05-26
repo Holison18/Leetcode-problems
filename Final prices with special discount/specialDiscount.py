6822acfe871a1b729946ac777651ed0a7519a885
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-05-26 14:32:33 +0000

    final price list with special discount

--- Final prices with special discount/specialDiscount.py
+++ Final prices with special discount/specialDiscount.py
@@ -1,14 +1,18 @@
 class Solution(object):
     def finalPrices(self, prices: list[int]) -> list[int]:
-        """returns the final price discount list"""
+        """Returns the final price discount list"""
         final_prices = []
-        for price in prices:
-            if price == prices[-1]:
-                final_prices.append(price)
-            if prices[price + 1] <= prices[price + 2]:
-                discount = prices[price] - prices[price + 1]
-                final_prices.append(discount)
-            else:
-                discount = prices[price] - prices[price + 2]
-                final_prices.append(discount)
+        n = len(prices)
+        for i in range(n):
+            discount = 0
+            for j in range(i + 1, n):
+                if prices[j] <= prices[i]:
+                    discount = prices[j]
+                    break
+            final_prices.append(prices[i] - discount)
         return final_prices
+    
+mySoln = Solution()
+print(mySoln.finalPrices([8,4,6,2,3]))
+print(mySoln.finalPrices([1,2,3,4,5]))
+print(mySoln.finalPrices([10,1,1,6]))

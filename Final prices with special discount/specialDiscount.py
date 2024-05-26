b4a56508b4b5dc21008ce4e6b626c6c0de49b5b1
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-05-26 14:06:32 +0000

    final list with special prices

--- Final prices with special discount/specialDiscount.py
+++ Final prices with special discount/specialDiscount.py
@@ -1,10 +1,14 @@
 class Solution(object):
-     def finalPrices(self, prices: list[int]) -> list[int]:
-          """returns the final price discount list"""
-          final_prices = []
-          for price in prices:
-               if price == prices[-1]:
-                    final_prices.append(price)
-               else:
-                    pass
-                    
+    def finalPrices(self, prices: list[int]) -> list[int]:
+        """returns the final price discount list"""
+        final_prices = []
+        for price in prices:
+            if price == prices[-1]:
+                final_prices.append(price)
+            if prices[price + 1] <= prices[price + 2]:
+                discount = prices[price] - prices[price + 1]
+                final_prices.append(discount)
+            else:
+                discount = prices[price] - prices[price + 2]
+                final_prices.append(discount)
+        return final_prices

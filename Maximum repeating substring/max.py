2f87343e654ee869035b77b8fd8898fc17d0f0d7
Kobina Akofi-Holison <80298246+Holison18@users.noreply.github.com>
2024-05-28 05:44:03 +0000

    Maximum repeating substring

--- Maximum repeating substring/max.py
+++ Maximum repeating substring/max.py
@@ -1,4 +1,11 @@
 class Solution(object):
     """To find the maximum repeating substring"""
     def maxRepeating(self, sequence: str, word: str) -> int:
-        pass
+        maximum_repeating = sequence.count(word)
+        return maximum_repeating
+    
+
+mySoln = Solution()
+print(mySoln.maxRepeating("ababc","ab"))
+print(mySoln.maxRepeating("ababc","ba"))
+print(mySoln.maxRepeating("ababc","ac"))
